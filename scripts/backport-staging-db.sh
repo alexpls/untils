620f3f89a514e4c22928c566ae93a03429bf5dde
Alex Plescan <alexpls@fastmail.com>
2026-02-18 20:42:33 +1000

    backport script drops db. dangerzone

--- scripts/backport-staging-db.sh
+++ scripts/backport-staging-db.sh
@@ -20,11 +20,23 @@ LOCAL_DB_PORT="54324"
 LOCAL_DB_USER="root"
 LOCAL_DB_PASSWORD="root"
 LOCAL_DB_NAME="untils_dev"
+LOCAL_DB_ADMIN_DB="postgres"
 
 DUMP_FILE="/tmp/untils_staging_dump_$$.sql"
 
 echo "==> Dumping staging database from ${REMOTE_HOST}..."
 ssh "${REMOTE_HOST}" "sudo docker exec ${REMOTE_CONTAINER} pg_dump -U ${REMOTE_DB_USER} -d ${REMOTE_DB_NAME} --clean --if-exists" > "${DUMP_FILE}"
+
+echo "==> Recreating local database ${LOCAL_DB_NAME}..."
+PGPASSWORD="${LOCAL_DB_PASSWORD}" psql \
+    -h "${LOCAL_DB_HOST}" \
+    -p "${LOCAL_DB_PORT}" \
+    -U "${LOCAL_DB_USER}" \
+    -d "${LOCAL_DB_ADMIN_DB}" \
+    -v ON_ERROR_STOP=1 \
+    -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '${LOCAL_DB_NAME}' and pid <> pg_backend_pid();" \
+    -c "drop database if exists ${LOCAL_DB_NAME};" \
+    -c "create database ${LOCAL_DB_NAME};"
 
 echo "==> Importing dump to local database..."
 PGPASSWORD="${LOCAL_DB_PASSWORD}" psql \
